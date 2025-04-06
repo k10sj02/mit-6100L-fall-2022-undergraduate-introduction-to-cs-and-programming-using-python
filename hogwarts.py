79a27ff8946db6d382e3e4bcf8bc1fcf430487e4
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-04-06 00:32:57 -0500

    updated notes

---
+++ hogwarts.py
@@ -0,0 +1,6 @@
+# students = ["Hermione", "Harry", "Ron", "Draco"]
+# house = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+
+# for i in range(len(students)):
+#   print(i + 1, students[i])
+
