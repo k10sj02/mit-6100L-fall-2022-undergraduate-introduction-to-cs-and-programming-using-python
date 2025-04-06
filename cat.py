49f984e67c3bc018db2006f8d556e0a54dfb52cc
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-04-05 23:20:07 -0500

    updated notes

--- cat.py
+++ cat.py
@@ -1,9 +1,27 @@
-#Run from the CLI using the command: python cat.py 
+# #Run from the CLI using the command: python cat.py 
 
-while True:
-  n = int(input("What's n? "))
-  if n > 0:
+# while True:
+#   n = int(input("What's n? "))
+#   if n > 0:
+#       break
+
+# for _ in range(n):
+#   print("meow")
+
+def main():
+  number = get_number()
+  meow(number)
+
+def get_number():
+  while True:
+    n = int(input("What's n? "))
+    if n > 0:
       break
+  return n
 
-for _ in range(n):
-  print("meow")
+def meow(n):
+  for _ in range(n):
+    print("meow")
+
+if __name__ == "__main__":
+    main()
