9244a1a18bd8b77c4c2a41b9c60e2736e1120785
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-04-05 22:25:51 -0500

    updated notes

--- cat.py
+++ cat.py
@@ -1,5 +1,9 @@
 #Run from the CLI using the command: python cat.py 
 
-print("meow")
-print("meow")
-print("meow")
+while True:
+  n = int(input("What's n? "))
+  if n > 0:
+      break
+
+for _ in range(n):
+  print("meow")
