3234c5a864ccde914cd084af7a76570cb43957f0
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-05-04 14:15:00 -0500

    folder reorg

--- core_mit_cs50/problem_sets/libraries/name.py
+++ core_mit_cs50/problem_sets/libraries/name.py
@@ -1,5 +1,7 @@
 import sys 
 
+# ATTEMPT 1 
+
 # if len(sys.argv) < 2:
 #   print("Too few arguments")
 # elif len(sys.argv) > 2:
@@ -7,11 +9,21 @@ import sys
 # else:
 #   print("hello, my name is", sys.argv[1])
 
-# Check for errors
+# ATTEMPT 2 (more elegant)
+# # Check for errors
+# if len(sys.argv) < 2:
+#   sys.exit("Too few arguments")
+# elif len(sys.argv) > 2:
+#   sys.exit("Too many arguments")
+
+# # Print name tags
+#   print("hello, my name is", sys.argv[1])
+
+# ATTEMPT 3 (even more elegant)
+
 if len(sys.argv) < 2:
   sys.exit("Too few arguments")
-elif len(sys.argv) > 2:
-  sys.exit("Too many arguments")
 
 # Print name tags
-  print("hello, my name is", sys.argv[1])
+for arg in sys.argv[1:]:
+  print("hello, my name is", arg)
