b2d8e625800c6d6134d40f03f4d639c575888854
Stann-Omar Jones <35823259+k10sj02@users.noreply.github.com>
2025-05-04 19:08:42 -0500

    folder reorg

--- core_mit_cs50/problem_sets/libraries/say.py
+++ core_mit_cs50/problem_sets/libraries/say.py
@@ -1,9 +1,15 @@
-import cowsay
-import sys
+# import cowsay
+# import sys
+
+# # if len(sys.argv) == 2:
+# #   cowsay.cow("hello, " + sys.argv[1])
+
 
 # if len(sys.argv) == 2:
-#   cowsay.cow("hello, " + sys.argv[1])
+#   cowsay.trex("hello, " + sys.argv[1])
 
+import sys
+from sayings import hello
 
 if len(sys.argv) == 2:
-  cowsay.trex("hello, " + sys.argv[1])
+  hello(sys.argv[1])
